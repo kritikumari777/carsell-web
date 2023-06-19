72f0e7c49a8cb0f41bc68d79886dd77e3d8700da
Kriti <123kritikumari@gmail.com>
2023-06-19 16:46:00 +0530

    Initial Commit

---
+++ components/index.tsx
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomeButton from "./CustomeButton";
+import Navebar from "./Navebar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  SearchManufacturer,
+  SearchBar,
+  CustomFilter,
+  Hero,
+  CustomeButton,
+  Navebar,
+  Footer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
